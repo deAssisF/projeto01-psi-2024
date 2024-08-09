1030f4b29ded1518191d3df89046a13dce798b6c
Francisco Cledson <franciscocledsof@gmail.com>
2024-08-09 07:26:18 -0300

    Mudanças no Django

--- futebolis/urls.py
+++ futebolis/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("jogadores/", views.jogadores, name="jogadores"),
+    path("sobre/", views.sobre, name="sobre"),
 ]
--- futebolis/views.py
+++ futebolis/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+def jogadores(request):
+    return render(request, "jogadores.html")
+def sobre(request):
+    return render(request, "sobre.html")
